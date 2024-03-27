1adc50021b71c785f8e4530a254a7b24342c56f5
kazilfr <kazilfr@gmail.com>
2024-03-27 13:43:42 -0400

    Switch To Expo Image

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, Image, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
 import { Img_path } from '../apicalls/apicalls';
 
 const MovieCard = ({ movie}) => {
   return (
     <View style={styles.container}>
-      <Image source={{ uri: Img_path + movie.poster_path }} style={styles.poster} />
+      <Image source={Img_path + movie.poster_path} style={styles.poster} />
     <Text style={styles.title}>{movie.title.length > 18 ? movie.title.slice(0, 16) + '...' : movie.title}</Text>
     </View>
   );
